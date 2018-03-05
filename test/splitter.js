746c18934f8caa6767712efb8d4099f1b03471e6
Sergii Kravchenko <mestrecorsaire@gmail.com>
2018-03-05 11:57:20 +0200

    refactoring

--- test/splitter.js
+++ test/splitter.js
@@ -6,18 +6,18 @@ contract('Splitter', function(accounts) {
   
     var add = 10000000;
 
-    assert(web3.eth.accounts.length>=3, "not enought acccounts to test");
-    assert(web3.eth.getBalance(web3.eth.accounts[0])> add, "first account does not have enought money to send 0.001 Ether");
+    assert(accounts.length>=3, "not enought acccounts to test");
+    assert(web3.eth.getBalance(accounts[0])> add, "first account does not have enought money to send 0.001 Ether");
      
     var initBalance = []
     for(i=0;i<3;i++)    
-      initBalance.push(web3.eth.getBalance(web3.eth.accounts[i]));
+      initBalance.push(web3.eth.getBalance(accounts[i]));
     
     return Splitter.deployed().then(function(instance) {
-       return instance.sendEther.sendTransaction(web3.eth.accounts[1], web3.eth.accounts[2], { from: web3.eth.accounts[0], value: add});
+       return instance.sendEther.sendTransaction(accounts[1], accounts[2], { from: accounts[0], value: add});
     }).then(function() {
-      assert.equal(web3.eth.getBalance(web3.eth.accounts[1]).toString(), initBalance[1].plus(add/2).toString(), "Bob failed");
-      assert.equal(web3.eth.getBalance(web3.eth.accounts[2]).toString(), initBalance[2].plus(add/2).toString(), "Carol failed");
+      assert.equal(web3.eth.getBalance(accounts[1]).toString(), initBalance[1].plus(add/2).toString(), "Bob failed");
+      assert.equal(web3.eth.getBalance(accounts[2]).toString(), initBalance[2].plus(add/2).toString(), "Carol failed");
     });
   });
 
